4abad649ed8d94765ff5ebc9fcda870b7adafda3
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-10-26 13:00:44 -0700

    chore: complete types

--- src/screens/Onboarding/CountryTypes.ts
+++ src/screens/Onboarding/CountryTypes.ts
@@ -2,27 +2,29 @@ import React from 'react';
 
 export type SocialSignupProps = {
   icon: React.ReactNode;
-  onPress: () => void;
+  onPress?: () => void;
 };
 
 export type CountryProps = {
-  code: number;
-  // short_name: string;
+  code: string;
+  short_name: string;
   name?: string;
+  id?: string;
+  imageLink?: string;
   onPress?: () => void;
 };
 
 export type CountriesType = Omit<CountryProps, 'onPress'>;
 
-export const Countries: CountriesType[] = [
+export const CountryDetails: CountriesType[] = [
   {
-    code: 234,
-    // short_name: 'NGN',
+    code: '+234',
+    short_name: 'NGN',
     name: 'Nigeria',
   },
   {
-    code: 233,
-    // short_name: 'NGN',
+    code: '+233',
+    short_name: 'GH',
     name: 'Ghana',
   },
 ];
