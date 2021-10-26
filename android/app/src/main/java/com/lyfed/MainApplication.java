5c3550baef989e3261119c03a0cf8b72f9452814
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-10-26 12:56:08 -0700

    chore: app set up

--- android/app/src/main/java/com/lyfed/MainApplication.java
+++ android/app/src/main/java/com/lyfed/MainApplication.java
@@ -6,6 +6,9 @@ import com.facebook.react.bridge.JSIModulePackage; // <- add
 import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.horcrux.svg.SvgPackage;
+import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
