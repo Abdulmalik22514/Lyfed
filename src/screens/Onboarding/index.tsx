a8d750fc4a8575924ac6a8b1b40c88e149452eca
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-11-09 22:22:01 +0100

    chore: refactor onboarding screen

--- src/screens/Onboarding/index.tsx
+++ src/screens/Onboarding/index.tsx
@@ -1,58 +1,22 @@
 import React, {useRef, useState} from 'react';
-import {
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  Alert,
-  FlatList,
-} from 'react-native';
+import {Text, View, Image, Alert, FlatList} from 'react-native';
 import * as Colors from '../../common/colors';
 import AppIntroSlider from 'react-native-app-intro-slider';
-import * as Images from '../../../assets/images';
 import {Button} from '../../common/buttons';
 import {OnboardingStyles as styles} from './style';
 import Modal from 'react-native-modal';
 import BottomSheet from '@gorhom/bottom-sheet';
-import {CountryDetails, CountriesType, CountryProps} from './CountryTypes';
-import {CountryBox} from './components/CountryInput';
-import {SocialSignupCard, SocialSignupList} from './components/SignupButtons';
+import {CountriesType, CountryProps, ItemProps} from './types';
 import {CountriesCard} from './components/CountriesCard';
-import {Header} from '../../common/header';
 import OTPInputView from '@twotalltotems/react-native-otp-input';
 import {Agreement} from '../../common/agreement';
-import {hp} from '../../common/utils';
-import {DropDown, UploadPicture} from '../../../assets/Svg';
-import {Input} from '../../common/input';
-import {DateContainer} from '../../common/dateBox';
-import {TextHeader} from '../../common/text';
 import {useCountries} from '../../hooks/useCountries';
-
-type ItemProps = {
-  key: number;
-  image: any;
-};
-
-type RenderItemProps = ItemProps[];
-
-const slides: RenderItemProps = [
-  {
-    key: 1,
-    image: Images.Onboarding2,
-  },
-  {
-    key: 2,
-    image: Images.Onboarding2,
-  },
-  {
-    key: 3,
-    image: Images.Onboarding2,
-  },
-  {
-    key: 4,
-    image: Images.Onboarding2,
-  },
-];
+import SignupStage1 from './SignupStage1';
+import {CountryDetails, SLIDES} from './constant';
+import SignupStage2 from './SignupStage2';
+import SignupStage3 from './SignupStage3';
+import {SignupStage4} from './SignupStage4';
+import {hp, wp} from '../../common/utils';
 
 export default function Onboarding() {
   const [modalVisible, setModalVisible] = useState(false);
@@ -140,7 +104,7 @@ export default function Onboarding() {
             setNext(curent + 1);
           }}
           keyExtractor={item => item.key.toString()}
-          data={slides}
+          data={SLIDES}
           renderItem={_renderItem}
           dotStyle={{backgroundColor: Colors.Border}}
           activeDotStyle={styles.activeDotStyle}
@@ -159,147 +123,34 @@ export default function Onboarding() {
           ref={bottomSheetRef}
           snapPoints={signupStage === 1 ? ['70%'] : ['80%']}>
           {signupStage === 1 && (
-            <>
-              <Header
-                heading="Enter your phone number"
-                description="We will send you a 4-digit OTP to your phone number for verification"
-              />
-              <CountryBox
-                onPress={() => setModalVisible(true)}
-                short_name={country.short_name}
-                code={country.code}
-                value={phoneNumber}
-                onChangeText={setPhoneNumber}
-                onSubmitEditing={handlePhoneLength}
-              />
-              <Button
-                title="send OTP"
-                style={styles.button}
-                styleText={styles.sendOTPButton}
-                onPressButton={handlePhoneLength}
-              />
-              <Text style={styles.orText}>Or</Text>
-              <View style={styles.signupOptions}>
-                {SocialSignupList.map((item, index) => {
-                  return (
-                    <SocialSignupCard
-                      key={index}
-                      icon={item.icon}
-                      onPress={() => setSignupStage(3)}
-                    />
-                  );
-                })}
-              </View>
-              <Agreement />
-            </>
+            <SignupStage1
+              onChangeText={setPhoneNumber}
+              onSendOtp={handlePhoneLength}
+              country={country}
+              phoneNumber={phoneNumber}
+              onCountryPress={() => setModalVisible(true)}
+            />
           )}
           {signupStage === 2 && (
-            <>
-              <Header
-                heading="Verify OTP"
-                description={`Please enter the 4-digit OTP sent to you at +234-${phoneNumber}`}
-              />
-              <TouchableOpacity
-                activeOpacity={0.7}
-                onPress={() => {
-                  setSignupStage(1);
-                }}>
-                <Text style={styles.editNumber}>Wrong number? Edit</Text>
-              </TouchableOpacity>
-              <OTPInputView
-                style={styles.otpInput}
-                pinCount={4}
-                placeholderCharacter="-"
-                code={code} //You can supply this prop or not. The component will be used as a controlled / uncontrolled component respectively.
-                onCodeChanged={() => {
-                  setCode(code);
-                }}
-                autoFocusOnLoad
-                codeInputFieldStyle={styles.underlineStyleBase}
-                codeInputHighlightStyle={styles.underlineStyleHighLighted}
-                onCodeFilled={code => {
-                  console.log(`Code is ${code}, you are good to go!`);
-                }}
-              />
-              <Button
-                title="Verify & continue"
-                style={styles.verifyButton}
-                styleText={styles.verifyButtonTitle}
-                onPressButton={() => setSignupStage(3)}
-              />
-              <Text style={styles.noOtp}>
-                Didn't get the OTP?{' '}
-                <Text style={styles.resend} onPress={() => setSignupStage(2)}>
-                  Resend
-                </Text>
-              </Text>
-
-              <Agreement />
-            </>
+            <SignupStage2
+              onWrongNumber={() => setSignupStage(1)}
+              phoneNumber={phoneNumber}
+              otpCode={code}
+              onOtpChanged={() => setCode(code)}
+              onVerify={() => setSignupStage(3)}
+              onResend={() => setSignupStage(2)}
+            />
           )}
           {signupStage === 3 && (
-            <>
-              <Header
-                heading="SignUp."
-                description="At Lyfed, we give information about Best Deals, Events, curated offers, sales & assured cashback for our customers."
-              />
-              <View style={{marginTop: hp(20)}}>
-                <Text style={styles.profilePicture}>
-                  Profile picture{' '}
-                  <Text style={styles.optionalText}>(optional)</Text>
-                </Text>
-
-                <TouchableOpacity
-                  onPress={() => setSignupStage(2)}
-                  style={styles.pictureContainer}
-                  activeOpacity={0.7}>
-                  <UploadPicture />
-                  <Text style={styles.pictureText}>Upload picture</Text>
-                </TouchableOpacity>
-                <Input label="Name" placeholder="Your name" />
-                <View style={{marginTop: hp(15)}}>
-                  <TextHeader label="Date of birth" />
-                  <View style={styles.dateContainer}>
-                    <DateContainer label="DD" />
-                    <DateContainer label="MM" />
-                    <DateContainer label="YYYY" isYear />
-                  </View>
-                </View>
-                <Button
-                  title="Next"
-                  style={styles.signupButton}
-                  styleText={styles.nextStage3}
-                  onPressButton={() => setSignupStage(4)}
-                />
-              </View>
-              <Agreement />
-            </>
+            <SignupStage3
+              onBack={() => setSignupStage(2)}
+              onNext={() => setSignupStage(4)}
+            />
           )}
           {signupStage === 4 && (
-            <>
-              <Header
-                style={styles.detailsHeader}
-                heading="A few more details."
-                description="Please fill in the following boxes to complete your registration to get started."
-              />
-              <TextHeader label="Gender" />
-              <View style={styles.genderSelection}>
-                <Text style={styles.gender}>Select gender</Text>
-                <TouchableOpacity activeOpacity={0.7}>
-                  <DropDown />
-                </TouchableOpacity>
-              </View>
-              <Input label="Email address" placeholder="Your email address" />
-              <Input label="Phone number" isPhone />
-              <Button
-                title="Create new account"
-                style={styles.accountButton}
-                styleText={styles.textStyle}
-                onPressButton={() => setSignupStage(3)}
-              />
-              <Agreement />
-            </>
+            <SignupStage4 onPressCreate={() => setSignupStage(3)} />
           )}
+          <Agreement />
         </BottomSheet>
         <Modal isVisible={modalVisible} hasBackdrop backdropOpacity={0.7}>
           <View>
